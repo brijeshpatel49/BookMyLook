207d965b90174b1a7fa4dab066aea8da72cc3490
brijeshpatel49 <134571757+brijeshpatel49@users.noreply.github.com>
2025-09-07 10:15:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/salon", salonRouter);
